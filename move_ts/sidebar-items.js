af634b4cefac8ba9c92c292c49afa2d3b5845f4f
macalinao <github-primary@igm.pub>
2022-07-05 23:04:12 +0000

    Deploying to gh-pages from @ movingco/move-ts@60a4d2b26e4d71065f6f2b2fb402c866466a6125 🚀

--- move_ts/sidebar-items.js
+++ move_ts/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["format",""],["idl_module",""],["idl_struct",""],["idl_type",""],["script_function",""]],"struct":[["CodeText",""],["CodegenContext",""]],"trait":[["Codegen","Generate TypeScript code for a value."]]});
+initSidebarItems({"fn":[["generate_index","Generates an `index.ts` file for the given module names."]],"mod":[["format",""],["idl_module",""],["idl_struct",""],["idl_type",""],["script_function",""]],"struct":[["CodeText",""],["CodegenContext",""]],"trait":[["Codegen","Generate TypeScript code for a value."]]});
