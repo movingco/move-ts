f3b4a039af14cd35edaaec4714257a0d98795211
Ian Macalinao <ian@moving.wtf>
2022-07-22 23:10:57 -0500

    update export names

--- crates/move-ts/src/idl_package.rs
+++ crates/move-ts/src/idl_package.rs
@@ -1,16 +1,22 @@
 use anyhow::*;
+use heck::ToSnakeCase;
 use move_idl::{IDLModule, IDLPackage};
 
 use crate::{idl_module::IDLModuleGenerator, CodeText, CodegenContext};
 
 /// Generates the module re-exports for the given module names.
-pub fn generate_module_reexports<'a, I>(module_names: I) -> Result<CodeText>
+pub fn generate_module_reexports<'a, I>(prefix: &str, module_names: I) -> Result<CodeText>
 where
     I: IntoIterator<Item = &'a String>,
 {
     Ok(module_names
         .into_iter()
-        .map(|name| format!("export * as {}Module from \"./{}/index.js\";", name, name))
+        .map(|name| {
+            format!(
+                "export * as {}_{} from \"./{}/index.js\";",
+                prefix, name, name
+            )
+        })
         .collect::<Vec<_>>()
         .join("\n")
         .into())
@@ -40,16 +46,18 @@ impl<'info> IDLPackageGenerator<'info> {
     }
 
     pub fn generate_index(&self) -> Result<CodeText> {
+        let prefix = &self.idl.name.to_snake_case();
         let index: CodeText = format!(
             "{}\n{}",
             generate_module_reexports(
+                prefix,
                 &self
                     .modules_to_generate
                     .iter()
                     .map(|m| m.module_id.name().to_string())
                     .collect::<Vec<_>>(),
             )?,
-            CodeText::new_named_reexport("errmap", "./errmap.js")
+            CodeText::new_named_reexport(&format!("errmap as {}_errmap", prefix), "./errmap.js")
         )
         .into();
 
